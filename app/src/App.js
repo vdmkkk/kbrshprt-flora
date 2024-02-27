37c1cf372be218d888583365c45c95f91931bb5f
zzil2 <zzil2.0@mail.ru>
2024-02-27 11:05:48 +0300

    bruh

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import './Widgets/Header'
 import Header from './Widgets/Header';
 import HeroSection from './Widgets/HeroSection';
 
 function App() {
   return (
-    <Header/>,
-    <HeroSection/> 
+    <><Header />
+    <HeroSection /></>
   )
 }
 
--- app/src/Widgets/HeroSection.js
+++ app/src/Widgets/HeroSection.js
@@ -7,8 +7,11 @@ function HeroSection() {
             <h1 className='Title'>Discover a place <br/> you will love to live</h1>
             <p className='Description'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
 Imperdiet tempus felis vitae sit est quisque.</p>  
+            <p className='Info'>Location Price Type of Property</p> {/* доделать! */}
+            <p className='Numbers'>2000+ 500+</p>
+            <p className='Under_nums'>Property Ready Happy Customer</p>
             <img className='Image' src={House} alt="picture" />     
-            <p className='Info'>Location Price Type of Property</p>   
+               
         </div>
 )}
 
